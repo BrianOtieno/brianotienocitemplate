62586dac8def98308ed4ca90d3f15470e41691aa
Brian Otieno <gebryo@intelligencia.com>
2016-10-09 16:13:02 +0300

    Update config.php
    Changed based url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/brianotienocitemplate/';
+$config['base_url']	= 'http://localhost/brianotienocitemplate/';
 
 /*
 |--------------------------------------------------------------------------
